1fb3b25c7619c6609854d59541a7de50c2ed540e
Tristan McCrea <papijackwaiter@gmail.com>
2026-08-08 08:04:15 -0400

    Finishing queries (besides bonus)
    Queries 15-25 complete. Bonus question and beautifying queries.sql remain

--- queries.sql
+++ queries.sql
@@ -20,23 +20,24 @@ WHERE name LIKE 'S____'
    OR name LIKE '%land%';
 
 -- 4. What countries are above average in population and area?
-SELECT c.name, i.population, c.area
+SELECT c.name, population, area
 FROM countries c
 JOIN indicators i ON c.iso_code=i.iso_code
-WHERE c.area > (SELECT AVG(area) FROM countries)
-AND i.population > (SELECT AVG(population) FROM indicators)
-ORDER BY i.population DESC;
+WHERE area > (SELECT AVG(area) FROM countries)
+AND population > (SELECT AVG(population) FROM indicators)
+ORDER BY population DESC;
 
 -- 5. Which country has the highest GDP in each region? (ALL not supported)
-SELECT c.region, c.name, e.gdp
+SELECT region, c.name, gdp
 FROM countries c
 JOIN economy e ON c.iso_code=e.iso_code
-WHERE e.gdp = (
+WHERE gdp = (
     SELECT MAX(e2.gdp)
     FROM countries c2 
     JOIN economy e2 ON c2.iso_code=e2.iso_code
     WHERE c2.region = c.region
-);
+)
+ORDER BY gdp DESC;
 
 -- 6. What are the 10 most densely populated countries?
 SELECT name, density
@@ -104,26 +105,114 @@ FROM countries c
 JOIN economy e ON c.iso_code=e.iso_code;
 
 -- 15. How many major cities does each region have, and what is their combined population?
+SELECT region, COUNT(name), SUM(population)
+FROM cities
+GROUP BY(region)
+ORDER BY COUNT(name) DESC;
 
 -- 16. Which countries have an HDI above the global average?
+SELECT c.name, hdi
+FROM countries c
+JOIN indicators i ON c.iso_code=i.iso_code
+WHERE hdi > (SELECT AVG(hdi) FROM indicators)
+ORDER BY hdi DESC;
 
 -- 17. Which development tier does each country fall into based on their HDI?
+SELECT c.name, hdi,
+CASE 
+    WHEN hdi IS NULL THEN 'No data'
+    WHEN hdi < 0.550 THEN  'Low Human Development'
+    WHEN hdi < 0.700 THEN  'Medium Human Development'
+    WHEN hdi < 0.800 THEN  'High Human Development'
+    ELSE 'Very High Human Development'
+END AS hdi_tier
+FROM countries c 
+JOIN indicators i ON c.iso_code=i.iso_code;
 
--- 18. Which countries have missing data for both HDI and GDP?
+-- 18. What cities are both their country's capital, and largest city?
+SELECT country, name, population
+FROM cities c1
+WHERE feature_code = 'PPLC' AND population >= (SELECT MAX(population) FROM cities c2 WHERE c1.country=c2.country);
 
 -- 19. Which countries have a birth rate higher than their region's average?
+SELECT region, c.name, birth_rate
+FROM countries c
+JOIN indicators i ON c.iso_code=i.iso_code
+WHERE birth_rate = (
+    SELECT MAX(i2.birth_rate)
+    FROM countries c2 
+    JOIN indicators i2 ON c2.iso_code=i2.iso_code
+    WHERE c2.region = c.region
+)
+ORDER BY(birth_rate) DESC;
 
 -- 20. What are the 5 most populated cities in each region?
+SELECT co.region, co.name AS country, ci.name AS city, population
+FROM countries co
+JOIN cities ci ON co.iso_code=ci.iso2
+WHERE (
+    SELECT COUNT(*)
+    FROM countries co2 
+    JOIN cities ci2 ON co2.iso_code=ci2.iso2
+    WHERE co2.region = co.region
+      AND ci2.population > ci.population
+) < 5
+ORDER BY co.region, population DESC;
 
 -- 21. What are the smallest countries by area that still have a major city? (bottom 5)
+SELECT co.name, ci.name, ci.population, area
+FROM countries co
+JOIN cities ci ON co.iso_code=ci.iso2
+WHERE area IS NOT NULL
+GROUP BY(co.name)
+ORDER BY area
+LIMIT 10;
 
 -- 22. Which country in each region has the lowest life expectancy?
+SELECT region, c.name, life_expectancy
+FROM countries c
+JOIN indicators i ON c.iso_code=i.iso_code
+WHERE life_expectancy <= (
+    SELECT MIN(i2.life_expectancy)
+    FROM countries c2 
+    JOIN indicators i2 ON c2.iso_code=i2.iso_code
+    WHERE c2.region = c.region
+)
+ORDER BY life_expectancy;
 
--- 23. Which countries rank highly in HDI or GDP per capita, but not both?
+-- 23. Which countries are above average in HDI or GDP per capita, but not both?
+SELECT c.name, hdi, gdp_per_capita
+FROM countries c
+JOIN indicators i ON c.iso_code=i.iso_code
+JOIN economy e ON c.iso_code=e.iso_code
+WHERE (
+  hdi > (SELECT AVG(hdi) FROM indicators) AND
+  NOT gdp_per_capita > (SELECT AVG(gdp_per_capita) FROM economy)
+) OR (
+  NOT hdi > (SELECT AVG(hdi) FROM indicators) AND
+  gdp_per_capita > (SELECT AVG(gdp_per_capita) FROM economy)
+)
 
 -- 24. What development tier is each capital city's country in?
+SELECT c.name, c.country, hdi,
+CASE 
+    WHEN hdi IS NULL THEN 'No data'
+    WHEN hdi < 0.550 THEN  'Low Human Development'
+    WHEN hdi < 0.700 THEN  'Medium Human Development'
+    WHEN hdi < 0.800 THEN  'High Human Development'
+    ELSE 'Very High Human Development'
+END AS hdi_tier
+FROM cities c 
+JOIN indicators i ON c.iso2=i.iso_code
+WHERE feature_code = 'PPLC'
+ORDER BY hdi DESC;
 
--- 25. Which countries have a higher GDP than every country in South Asia?
+-- 25. Which countries have a higher GDP than every country in Africa combined?
+SELECT c.name, gdp
+FROM countries c
+JOIN economy e ON c.iso_code=e.iso_code
+WHERE gdp > (SELECT SUM(gdp) FROM countries c2 JOIN economy e2 ON c2.iso_code=e2.iso_code WHERE region = 'Africa')
+ORDER BY gdp DESC;
 
 -- BONUS. For each region, find countries that rank in the top 25% for GDP per capita, but in the bottom 25% for HDI within their region
 -- Display each country with its region, GDP per capita, HDI, an income classification label, and the average GDP per capita of its region for comparison
